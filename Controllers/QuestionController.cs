b575eb1d58a30efa0c6dcab4ff9863f7c294f640
FizaretOsmanov <fizaretgithub@gmail.com>
2024-12-28 11:04:00 +0400

    ...

---
+++ Controllers/QuestionController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CompetitionApp.Controllers
+{
+    public class QuestionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Index1()
+        {
+            return View();
+        }
+    }
+}
