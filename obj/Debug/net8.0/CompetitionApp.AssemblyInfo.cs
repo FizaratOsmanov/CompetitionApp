db8c969af93df5608d86d55197f2071a683ae823
FizaretOsmanov <fizaretgithub@gmail.com>
2025-01-03 14:04:12 +0400

    ...

--- obj/Debug/net8.0/CompetitionApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/CompetitionApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CompetitionApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0835dc65f5bad115aa425ed334100be333c30a57")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6e9a8e167cda0bc292f34e95bcf39c703a12266")]
 [assembly: System.Reflection.AssemblyProductAttribute("CompetitionApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CompetitionApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
