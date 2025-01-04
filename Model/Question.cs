a313590a064a8874e4cca7cf6cba97960298d734
FizaretOsmanov <fizaretgithub@gmail.com>
2025-01-04 20:05:24 +0400

    Az qaldi

--- Model/Question.cs
+++ Model/Question.cs
@@ -5,7 +5,8 @@
         public int Id { get; set; }
         public string Text { get; set; } 
         public List<string> Options { get; set; } 
-        public string ImageUrl { get; set; } 
+        public string ImageUrl { get; set; }
+        public string AudioUrl { get; set; }
         public string QuestionType { get; set; } 
 
         public string? CorrectAnswer { get; set; } 
